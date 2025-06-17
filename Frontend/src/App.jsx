abece3e5977c8303b2247a11150d8da18231b802
rajat <rajatmishra6767@gmail.com>
2025-06-17 15:00:16 +0530

    added main logo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Doctors from './pages/Doctors.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import MyAppointment from './pages/MyAppointment.jsx'
+import Appointment from './pages/Appointment.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar.jsx'
+
+
+
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-appointments' element={<MyAppointment/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+      </Routes>
      
     </div>
   )
